c332dd3bd33a44dcb81a83a566079311c0047c15
Edward Martinez <emartinezj1802@gmail.com>
2025-06-24 12:21:13 -0500

    se genera visualizacion de el campo ode un string y metodos

---
+++ d3p1.py
@@ -0,0 +1,27 @@
+mi_texto = "Esto es un a prueba"
+resultado = mi_texto[2]
+resultadoneg = mi_texto[-5]
+print(resultado)
+print(resultadoneg)
+
+
+"""
+se puede generar la visualizacion positiva o negativa  de la siguiente manera}
+
+POSITIVA
+Esto es una prueba
+012345679891111111
+           0123456
+inicia desde 0 con la leta "E" y termina con 17 que es la ultima letra "a"
+
+Esto es una prueba
+------------------ menos
+011111111987654321
+ 76543210
+
+"""
+
+#METODOS
+
+resultadometodo = mi_texto.index("e")
+print(resultadometodo)
